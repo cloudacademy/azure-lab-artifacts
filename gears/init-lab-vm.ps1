24b758160366ae9f5d8cecc6ff7dc06e6070cd5d
Logan Rakai <lrakai@users.noreply.github.com>
2022-01-05 22:45:02 -0700

    Set hosts

--- gears/init-lab-vm.ps1
+++ gears/init-lab-vm.ps1
@@ -1,1 +1,13 @@
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
+
+$HostFile = "$env:windir\system32\drivers\etc\hosts"
+Add-content -path $HostFile -value "192.168.1.11 `t DCA.root.contoso.com" 
+Add-content -path $HostFile -value "192.168.1.12 `t DCB.root.contoso.com" 
+Add-content -path $HostFile -value "192.168.1.21 `t Member01.root.contoso.com" 
+Add-content -path $HostFile -value "192.168.1.22 `t Member02.root.contoso.com" 
+Add-content -path $HostFile -value "192.168.1.23 `t Member03.root.contoso.com" 
+Add-content -path $HostFile -value "192.168.1.24 `t Member04.root.contoso.com" 
+Add-content -path $HostFile -value "192.168.1.32 `t Client02.root.contoso.com" 
+Add-content -path $HostFile -value "192.168.1.100 `t FABDC.fabrikam.com" 
+Add-content -path $HostFile -value "192.168.1.101 `t FABMember.fabrikam.com" 
+Add-content -path $HostFile -value "192.168.1.102 `t FABClient.fabrikam.com"  
